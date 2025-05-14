fe8035ea5d9518de9890bd4eb4136a256641ffb8
Nikolay Sivko <n.sivko@gmail.com>
2025-05-14 10:58:39 +0300

    enable k8s service discovery to scrape custom metrics from annotated pods

--- go.mod
+++ go.mod
@@ -60,6 +60,7 @@ require (
 	github.com/grafana/regexp v0.0.0-20221122212121-6b5c0a4cb7fd // indirect
 	github.com/grobie/gomemcache v0.0.0-20230213081705-239240bbc445 // indirect
 	github.com/hashicorp/go-version v1.6.0 // indirect
+	github.com/imdario/mergo v0.3.16 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/jpillora/backoff v1.0.0 // indirect
@@ -83,6 +84,7 @@ require (
 	github.com/prometheus/procfs v0.12.0 // indirect
 	github.com/rogpeppe/go-internal v1.11.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
--- metrics/scraper.go
+++ metrics/scraper.go
@@ -1,6 +1,9 @@
 package metrics
 
 import (
+	"context"
+	"errors"
+	"os"
 	"time"
 
 	"github.com/coroot/coroot-cluster-agent/common"
@@ -10,12 +13,15 @@ import (
 	promCommon "github.com/prometheus/common/config"
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/config"
-	"github.com/prometheus/prometheus/discovery/targetgroup"
+	"github.com/prometheus/prometheus/discovery"
+	"github.com/prometheus/prometheus/discovery/kubernetes"
+	"github.com/prometheus/prometheus/model/relabel"
 	"github.com/prometheus/prometheus/scrape"
 	"github.com/prometheus/prometheus/storage"
 	"github.com/prometheus/prometheus/storage/remote"
 	"github.com/prometheus/prometheus/tsdb"
 	"github.com/prometheus/prometheus/tsdb/agent"
+	"k8s.io/klog"
 )
 
 const (
@@ -40,6 +46,17 @@ func (ms *Metrics) runScraper() error {
 			},
 		},
 	)
+	targets := []model.LabelSet{{
+		model.AddressLabel:  model.LabelValue(ms.listenAddr),
+		model.InstanceLabel: model.LabelValue(ms.listenAddr),
+	}}
+	if ms.ksmAddr != "" {
+		targets = append(targets, model.LabelSet{
+			model.AddressLabel:  model.LabelValue(ms.ksmAddr),
+			model.InstanceLabel: model.LabelValue(ms.ksmAddr),
+		})
+	}
+
 	cfg.ScrapeConfigs = append(cfg.ScrapeConfigs, &config.ScrapeConfig{
 		JobName:                 jobName,
 		HonorLabels:             true,
@@ -47,7 +64,15 @@ func (ms *Metrics) runScraper() error {
 		MetricsPath:             "/metrics",
 		Scheme:                  "http",
 		EnableCompression:       false,
+		ServiceDiscoveryConfigs: []discovery.Config{
+			discovery.StaticConfig{{Targets: targets}},
+		},
 	})
+	if k8sCfg := k8sDiscovery(); k8sCfg != nil {
+		klog.Infoln("enabling k8s service discovery")
+		cfg.ScrapeConfigs = append(cfg.ScrapeConfigs, k8sCfg)
+	}
+
 	localStorage := &readyStorage{stats: tsdb.NewDBStats()}
 	scraper := &readyScrapeManager{}
 	remoteStorage := remote.NewStorage(logger, prometheus.DefaultRegisterer, localStorage.StartTime, ms.walDir, RemoteFlushDeadline, scraper)
@@ -56,6 +81,31 @@ func (ms *Metrics) runScraper() error {
 	if err := remoteStorage.ApplyConfig(&cfg); err != nil {
 		return err
 	}
+	sdMetrics, err := discovery.CreateAndRegisterSDMetrics(prometheus.DefaultRegisterer)
+	if err != nil {
+		return err
+	}
+	discMgr := discovery.NewManager(context.TODO(), logger, prometheus.DefaultRegisterer, sdMetrics, discovery.Name("scrape"))
+	if discMgr == nil {
+		return errors.New("could not create discovery manager")
+	}
+	c := make(map[string]discovery.Configs)
+	scfgs, err := cfg.GetScrapeConfigs()
+	if err != nil {
+		return err
+	}
+	for _, v := range scfgs {
+		c[v.JobName] = v.ServiceDiscoveryConfigs
+	}
+	if err = discMgr.ApplyConfig(c); err != nil {
+		return err
+	}
+
+	go func() {
+		if err = discMgr.Run(); err != nil {
+			klog.Exitln("error running discovery manager:", err)
+		}
+	}()
 
 	scrapeManager, err := scrape.NewManager(nil, logger, fanoutStorage, prometheus.DefaultRegisterer)
 	if err != nil {
@@ -71,29 +121,79 @@ func (ms *Metrics) runScraper() error {
 	}
 	localStorage.Set(db, 0)
 	db.SetWriteNotified(remoteStorage)
+	go func() {
+		if err = scrapeManager.Run(discMgr.SyncCh()); err != nil {
+			klog.Exitln("error running scrape manager:", err)
+		}
+	}()
+	return nil
+}
 
-	tch := make(chan map[string][]*targetgroup.Group)
-	go scrapeManager.Run(tch)
-	targets := []model.LabelSet{
-		{
-			model.AddressLabel:  model.LabelValue(ms.listenAddr),
-			model.InstanceLabel: model.LabelValue(ms.listenAddr),
+func k8sDiscovery() *config.ScrapeConfig {
+	host, port := os.Getenv("KUBERNETES_SERVICE_HOST"), os.Getenv("KUBERNETES_SERVICE_PORT")
+	if len(host) == 0 || len(port) == 0 {
+		klog.Infoln("not in k8s cluster, disabling k8s service discovery")
+		return nil
+	}
+	return &config.ScrapeConfig{
+		JobName:                 "custom-metrics-k8s-pods",
+		HonorLabels:             true,
+		ScrapeClassicHistograms: true,
+		MetricsPath:             "/metrics",
+		Scheme:                  "http",
+		EnableCompression:       false,
+		RelabelConfigs: []*relabel.Config{
+			{
+				SourceLabels: model.LabelNames{"__meta_kubernetes_pod_annotation_coroot_com_scrape_metrics"},
+				Action:       relabel.Keep,
+				Regex:        relabel.MustNewRegexp("true"),
+			},
+			{
+				SourceLabels: model.LabelNames{"__meta_kubernetes_pod_annotation_coroot_com_metrics_path"},
+				Action:       relabel.Replace,
+				TargetLabel:  "__metrics_path__",
+				Regex:        relabel.MustNewRegexp("(.+)"),
+				Replacement:  "$1",
+			},
+			{
+				SourceLabels: model.LabelNames{"__meta_kubernetes_pod_name"},
+				TargetLabel:  "pod",
+				Action:       relabel.Replace,
+				Regex:        relabel.MustNewRegexp("(.+)"),
+				Replacement:  "$1",
+			},
+			{
+				SourceLabels: model.LabelNames{"__meta_kubernetes_namespace"},
+				TargetLabel:  "namespace",
+				Action:       relabel.Replace,
+				Regex:        relabel.MustNewRegexp("(.+)"),
+				Replacement:  "$1",
+			},
+			{
+				SourceLabels: model.LabelNames{"__address__", "__meta_kubernetes_pod_annotation_coroot_com_metrics_port"},
+				TargetLabel:  "__address__",
+				Regex:        relabel.MustNewRegexp(`(.+?)(?::\d+)?;(\d+)`),
+				Replacement:  "$1:$2",
+				Action:       relabel.Replace,
+			},
+			{
+				SourceLabels: model.LabelNames{"__meta_kubernetes_service_annotation_coroot_com_metrics_scheme"},
+				TargetLabel:  "__scheme__",
+				Regex:        relabel.MustNewRegexp("(https?)"),
+				Replacement:  "$1",
+				Action:       relabel.Replace,
+			},
+			{
+				SourceLabels: model.LabelNames{"__meta_kubernetes_pod_phase"},
+				Regex:        relabel.MustNewRegexp("Pending|Succeeded|Failed|Completed"),
+				Action:       relabel.Drop,
+			},
 		},
-	}
-	if ms.ksmAddr != "" {
-		targets = append(targets, model.LabelSet{
-			model.AddressLabel:  model.LabelValue(ms.ksmAddr),
-			model.InstanceLabel: model.LabelValue(ms.ksmAddr),
-		})
-	}
-	tch <- map[string][]*targetgroup.Group{
-		jobName: {
-			&targetgroup.Group{
-				Targets: targets,
-				Labels:  model.LabelSet{model.JobLabel: jobName},
+		ServiceDiscoveryConfigs: []discovery.Config{
+			&kubernetes.SDConfig{
+				Role:      kubernetes.RolePod,
+				Selectors: []kubernetes.SelectorConfig{{Role: "pod"}},
 			},
 		},
 	}
-
-	return nil
 }
